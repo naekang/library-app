46078c7cc834841e3a3770243cc2373eb6c1f971
naekang <rlawlsgh6306@gmail.com>
2023-02-08 15:42:33 +0900

    feat: Add Enum type settings

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,15 +1,13 @@
 package com.group.libraryapp.domain.book
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Book(
 
     val name: String,
 
+    @Enumerated(EnumType.STRING)
     val type: BookType,
 
     @Id
