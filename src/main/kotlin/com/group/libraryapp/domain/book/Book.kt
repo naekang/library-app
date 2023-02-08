51f833acd294f1b1f7486c6bd6b611ab41acd154
naekang <rlawlsgh6306@gmail.com>
2023-02-08 15:38:17 +0900

    feat: Change BookType String to Enum class

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -10,7 +10,7 @@ class Book(
 
     val name: String,
 
-    val type: String,
+    val type: BookType,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +26,7 @@ class Book(
     companion object {
         fun fixture(
             name: String = "책 이름",
-            type: String = "COMPUTER",
+            type: BookType = BookType.COMPUTER,
             id: Long? = null,
         ): Book {
             return Book(
@@ -36,5 +36,4 @@ class Book(
             )
         }
     }
-
 }
