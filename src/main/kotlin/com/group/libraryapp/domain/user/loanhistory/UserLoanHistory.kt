2ffe5dfd28f6eccfc286f3947a5cbb7d3b9a213b
naekang <rlawlsgh6306@gmail.com>
2023-02-09 17:47:15 +0900

    refactor: Refactor with Static Factory Method

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -19,6 +19,9 @@ class UserLoanHistory(
 
     ) {
 
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
